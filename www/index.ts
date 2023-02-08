3b62d087cfb095d1a1eb947fc3ae715efe7229a3
Terry Luan <terryluan12@gmail.com>
2023-02-07 22:34:25 -0800

    Initial Universe using Unicode characters

--- www/index.ts
+++ www/index.ts
@@ -1,3 +1,14 @@
+import { Universe } from "wasm-game-of-life";
+
 export default function main(rust_wasm: any) {
-  rust_wasm.greet();
+  const pre = document.getElementById("game-of-life-canvas");
+  const universe = new Universe();
+
+  const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+  };
+  requestAnimationFrame(renderLoop);
 }
